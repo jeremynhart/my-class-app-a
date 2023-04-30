282ce544eeddd3f1cade35c55d00831a8da12fa8
Jeremy Hart <jeremynhart@gmail.com>
2023-04-30 12:49:29 -0500

    Gallery Component Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
+import React from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import './App.css';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
